5e2e54d4da7fcd6b55603b5863da6b9308902a69
Serge Boyko <boykodev@gmail.com>
2016-03-20 22:57:46 +0200

    Fix for street type

--- regex/20.js
+++ regex/20.js
@@ -4,17 +4,20 @@ let addresses = require('./addresses');
 
 /* Розбиття регулярки для зручності редагування */
 let patterns = {
-    streetName: /^\s*((?:ул|пр-т|вул|пл|пер)?[.\s]*(?:\d*[-])?[А-Яа-яЁёІіЇїЄєҐґ\-.\s]+)/,
-    houseType: /(?:[,\s]*(?:дом)?[.\s]*)?/,
+    streetType: /^\s*(?:ул|вул|пр-т|пл|пер)?[.\s]*/,
+    streetName: /((?:\d*[-])?[А-Яа-яЁёІіЇїЄєҐґ\-.\s]+)/,
+    houseType: /(?:[,\s]*(?:дом|дім|д.)?[.\s]*)?/,
     houseNumber: /(\d+[А-Яа-яЁёІіЇїЄєҐґ-]*)?/,
     flatType: /(?:[,\s]*(?:кв)?[.\s]*)?/,
     flatNumber: /\/?(\d+)?\s*$/
 };
 
 /* Конкатинація регулярки в один вираз */
-let pattern = new RegExp(patterns.streetName.source +
+let pattern = new RegExp(
+    patterns.streetType.source + patterns.streetName.source +
     patterns.houseType.source + patterns.houseNumber.source +
-    patterns.flatType.source + patterns.flatNumber.source);
+    patterns.flatType.source + patterns.flatNumber.source
+);
 
 /* Отримання результатів */
 let result = [];
